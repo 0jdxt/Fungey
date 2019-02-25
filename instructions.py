685f8904796537157d62a6c77d4496708f780ebf
0jdxt <jytrn@protonmail.com>
2019-02-25 15:57:38 +0000

    instructions tidy/refactor

--- instructions.py
+++ instructions.py
@@ -1,4 +1,4 @@
-from typing import Dict, Callable, List
+from typing import Dict, Callable, List, Union
 import sys
 
 from click import prompt, getchar as getch
@@ -6,187 +6,190 @@ from click import prompt, getchar as getch
 from classes import FungeSpace
 
 
-def cmd_unknown(space: FungeSpace) -> None:
+def _cmd_unknown(space: FungeSpace) -> None:
     print(f"unknown: {chr(space.val())}")
     sys.exit()
 
 
-def cmd_null(space: FungeSpace) -> None:
+def _cmd_null(space: FungeSpace) -> None:
     pass
 
 
-def cmd_stop(space: FungeSpace) -> None:
+def _cmd_stop(space: FungeSpace) -> None:
     space.running = False
 
 
-def cmd_jump(space: FungeSpace) -> None:
+def _cmd_jump(space: FungeSpace) -> None:
     space.move()
 
 
-def cmd_push_digit(space: FungeSpace) -> int:
+def _cmd_push_digit(space: FungeSpace) -> int:
     return int(chr(space.val()))
 
 
-def cmd_push_hex(space: FungeSpace) -> int:
+def _cmd_push_hex(space: FungeSpace) -> int:
     return space.val() - 86
 
 
-def cmd_maths_add(space: FungeSpace) -> int:
+def _cmd_maths_add(space: FungeSpace) -> int:
     return space.stack.pop() + space.stack.pop()
 
 
-def cmd_maths_mul(space: FungeSpace) -> int:
+def _cmd_maths_mul(space: FungeSpace) -> int:
     return space.stack.pop() * space.stack.pop()
 
 
-def cmd_maths_sub(space: FungeSpace) -> int:
+def _cmd_maths_sub(space: FungeSpace) -> int:
     a, b = space.stack.pop(2)
     return b - a
 
 
-def cmd_maths_div(space: FungeSpace) -> int:
+def _cmd_maths_div(space: FungeSpace) -> int:
     a, b = space.stack.pop(2)
     return b // a
 
 
-def cmd_maths_mod(space: FungeSpace) -> int:
+def _cmd_maths_mod(space: FungeSpace) -> int:
     a, b = space.stack.pop(2)
     return b % a
 
 
-def cmd_logic_not(space: FungeSpace) -> int:
+def _cmd_logic_not(space: FungeSpace) -> int:
     return int(not space.stack.pop())
 
 
-def cmd_logic_gt(space: FungeSpace) -> int:
-    a, b = space.stack.pop(), space.stack.pop()
+def _cmd_logic_gt(space: FungeSpace) -> int:
+    a, b = space.stack.pop(2)
     return int(b > a)
 
 
-def cmd_stack_dup(space: FungeSpace) -> List[int]:
+def _cmd_stack_dup(space: FungeSpace) -> List[int]:
     return [space.stack.pop()] * 2
 
 
-def cmd_stack_swp(space: FungeSpace) -> List[int]:
+def _cmd_stack_swp(space: FungeSpace) -> List[int]:
     return list(space.stack.pop(2))
 
 
-def cmd_stack_pop(space: FungeSpace) -> None:
+def _cmd_stack_pop(space: FungeSpace) -> None:
     space.stack.pop()
 
 
-def cmd_space_get(space: FungeSpace) -> int:
+def _cmd_space_get(space: FungeSpace) -> int:
     y, x = space.stack.pop(2)
     return space[x, y]
 
 
-def cmd_space_put(space: FungeSpace) -> None:
+def _cmd_space_put(space: FungeSpace) -> None:
     y, x, v = space.stack.pop(3)
     space[x, y] = v
 
 
-def cmd_drctn_N(space: FungeSpace) -> None:
+def _cmd_drctn_N(space: FungeSpace) -> None:
     space.ip.north()
 
 
-def cmd_drctn_S(space: FungeSpace) -> None:
+def _cmd_drctn_S(space: FungeSpace) -> None:
     space.ip.south()
 
 
-def cmd_drctn_E(space: FungeSpace) -> None:
+def _cmd_drctn_E(space: FungeSpace) -> None:
     space.ip.east()
 
 
-def cmd_drctn_W(space: FungeSpace) -> None:
+def _cmd_drctn_W(space: FungeSpace) -> None:
     space.ip.west()
 
 
-def cmd_drctn_rot_left(space: FungeSpace) -> None:
+def _cmd_drctn_rot_left(space: FungeSpace) -> None:
     space.ip.rotate(90)
 
 
-def cmd_drctn_rot_right(space: FungeSpace) -> None:
+def _cmd_drctn_rot_right(space: FungeSpace) -> None:
     space.ip.rotate(-90)
 
 
-def cmd_drctn_rand(space: FungeSpace) -> None:
+def _cmd_drctn_rand(space: FungeSpace) -> None:
     space.ip.random()
 
 
-def cmd_drctn_H_or(space: FungeSpace) -> None:
+def _cmd_drctn_H_or(space: FungeSpace) -> None:
     if space.stack.pop():
         space.ip.west()
     else:
         space.ip.east()
 
 
-def cmd_drctn_V_or(space: FungeSpace) -> None:
+def _cmd_drctn_V_or(space: FungeSpace) -> None:
     if space.stack.pop():
         space.ip.north()
     else:
         space.ip.south()
 
 
-def cmd_io_out_digit(space: FungeSpace) -> None:
+def _cmd_io_out_digit(space: FungeSpace) -> None:
     sys.stdout.write(f"{space.stack.pop()} ")
     sys.stdout.flush()
 
 
-def cmd_io_out_char(space: FungeSpace) -> None:
+def _cmd_io_out_char(space: FungeSpace) -> None:
     sys.stdout.write(chr(space.stack.pop()))
     sys.stdout.flush()
 
 
-def cmd_io_in_line(space: FungeSpace) -> int:
+def _cmd_io_in_line(space: FungeSpace) -> int:
     return int(input("=>"))
 
 
-def cmd_io_in_char(space: FungeSpace) -> int:
+def _cmd_io_in_char(space: FungeSpace) -> int:
     sys.stdout.write(">")
     sys.stdout.flush()
     return ord(getch())
 
 
-command_routes: Dict[str, Callable] = {
+_command_routes: Dict[str, Callable] = {
     # control
-    " ": cmd_null,
-    "#": cmd_jump,
-    "@": cmd_stop,
+    " ": _cmd_null,
+    "#": _cmd_jump,
+    "@": _cmd_stop,
     # binary ops
-    "+": cmd_maths_add,
-    "-": cmd_maths_sub,
-    "*": cmd_maths_mul,
-    "/": cmd_maths_div,
-    "%": cmd_maths_mod,
+    "+": _cmd_maths_add,
+    "-": _cmd_maths_sub,
+    "*": _cmd_maths_mul,
+    "/": _cmd_maths_div,
+    "%": _cmd_maths_mod,
     # logic
-    "!": cmd_logic_not,
-    "`": cmd_logic_gt,
+    "!": _cmd_logic_not,
+    "`": _cmd_logic_gt,
     # stack manipulation
-    ":": cmd_stack_dup,
-    "\\": cmd_stack_swp,
-    "$": cmd_stack_pop,
+    ":": _cmd_stack_dup,
+    "\\": _cmd_stack_swp,
+    "$": _cmd_stack_pop,
     # FungeSpace manipulation
-    "g": cmd_space_get,
-    "p": cmd_space_put,
+    "g": _cmd_space_get,
+    "p": _cmd_space_put,
     # pointer directions
-    "^": cmd_drctn_N,
-    "v": cmd_drctn_S,
-    ">": cmd_drctn_E,
-    "<": cmd_drctn_W,
-    "[": cmd_drctn_rot_left,
-    "]": cmd_drctn_rot_right,
-    "?": cmd_drctn_rand,
-    "_": cmd_drctn_H_or,
-    "|": cmd_drctn_V_or,
+    "^": _cmd_drctn_N,
+    "v": _cmd_drctn_S,
+    ">": _cmd_drctn_E,
+    "<": _cmd_drctn_W,
+    "[": _cmd_drctn_rot_left,
+    "]": _cmd_drctn_rot_right,
+    "?": _cmd_drctn_rand,
+    "_": _cmd_drctn_H_or,
+    "|": _cmd_drctn_V_or,
     # i/o
-    ".": cmd_io_out_digit,
-    ",": cmd_io_out_char,
-    "&": cmd_io_in_line,
-    "~": cmd_io_in_char,
+    ".": _cmd_io_out_digit,
+    ",": _cmd_io_out_char,
+    "&": _cmd_io_in_line,
+    "~": _cmd_io_in_char,
 }
-command_routes.update(dict.fromkeys("0123456789", cmd_push_digit))
-command_routes.update(dict.fromkeys("abcdef", cmd_push_hex))
+_command_routes.update(dict.fromkeys("0123456789", _cmd_push_digit))
+_command_routes.update(dict.fromkeys("abcdef", _cmd_push_hex))
 
 
-def get_instruction(char: str) -> Callable:
-    return command_routes.get(char, cmd_unknown)
+def run_instruction(char: str, space: FungeSpace) -> None:
+    """Get and run instruction. If value returned, push onto stack"""
+    res = _command_routes.get(char, _cmd_unknown)(space)
+    if res is not None:
+        space.stack.push(res)
--- interpreter.py
+++ interpreter.py
@@ -2,7 +2,7 @@
 import click
 from sys import stdout
 
-from instructions import get_instruction
+from instructions import run_instruction
 from classes import FungeSpace
 
 
@@ -15,22 +15,20 @@ def main(program: click.File, raw: bool = False) -> None:
     # TODO: logging, commenting
 
     # If no state activated, continue processing of char
-    # else, move to next instruction
+    # else, move to next cell
     while space.running:
         cmd = space.val()
         char = chr(cmd)
         # print(space.ip.loc(), ":", cmd, char, end=" ")
 
         if space.toggle_states(char):
-            # Get and run instruction. If value returned, push onto stack
-            res = get_instruction(char)(space)
-            if res is not None:
-                space.stack.push(res)
+            run_instruction(char, space)
 
         # print(space.stack)
         space.move()
         # print(space.ip.loc(), space.val())
 
+    # output newline to terminal unless --raw
     if (not raw) and stdout.isatty():
         click.echo("")
 
